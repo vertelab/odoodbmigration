31aa42d11da362001af38b8a30a6a038ee76d4bf
Lukas Pohlman <pohluk@barney.vertel.se>
2021-01-13 14:45:47 +0000

    Updated fields and creation

--- mariaprod.py
+++ mariaprod.py
@@ -1,62 +1,3 @@
-# import argparse
-# import json
-# import logging
-# import sys
-# try:
-#     import odoorpc
-# except ImportError:
-#     raise Warning('odoorpc library missing. Please install the library. Eg: pip3 install odoorpc')
-# #%% ---------------------------------------------------------------------------
-
-# source_params = {
-#             "host" : "localhost",
-#             "port" : 5080,
-#             "db"   : "dermanord",
-#             "user" : "admin",
-#             "password"  : "InwX11Je3DtifHHb"        
-#         }
-
-# target_params = {
-#             "host" : "81.170.214.150",
-#             "port" : 8069,
-#             "db"   : "maria_demo",
-#             "user" : "admin",
-#             "password"  : "PVLd2bXhfLHeqGir"        
-#         }
-
-# fields = [
-#               'active','description','description_sale',
-#               'default_code','name','list_price',
-#               'standard_price',
-#               'website_description',
-#               'website_published']
-
-# # nfields = fields + ['old_id']
-
-# source = odoorpc.ODOO(host=source_params["host"],port=source_params["port"])
-# source.login(source_params["db"],login=source_params["user"],password=source_params["password"])
-# target = odoorpc.ODOO(host=target_params["host"],port=target_params["port"])
-# target.login(target_params["db"],login=target_params["user"],password=target_params["password"])
-
-# parents=[]
-# print('hej')
-# # for target_id in target.env['product.template'].search([]):
-# #   target_product = target.env['product.template'].browse(target_id)
-#   # target_product.unlink()
-
-
-# # target_product = target.env['product.template'].create({'name': 'sandra'})
-# # print(target_product)
-
-# for source_id in source.env['product.product'].search([]):
-#   print ('hej')
-#   source_product = source.env['product.product'].read(source_id, fields)
-#   target_product = target.env['product.product'].create({key:source_product[key] for key in fields})
-#   # target_product = target.env['product.template'].create({key:source_product[key] for key in fields})
-#   # target_product.old_id = source_id
-
-
-
 import argparse
 import json
 import logging
@@ -65,7 +6,6 @@ try:
     import odoorpc
 except ImportError:
     raise Warning('odoorpc library missing. Please install the library. Eg: pip3 install odoorpc')
-#%% ---------------------------------------------------------------------------
 
 source_params = {
             "host" : "localhost",
@@ -78,14 +18,20 @@ source_params = {
 target_params = {
             "host" : "81.170.214.150",
             "port" : 8069,
-            "db"   : "maria_demo",
+            "db"   : "maria_portering",
             "user" : "admin",
-            "password"  : "PVLd2bXhfLHeqGir"        
+            "password"  : "admin"
         }
 
-
-fields = [
-              'name','list_price']
+# 'source_field_name' : 'target_field_name'
+fields = {'name', 'name',
+    'active' : 'active',
+    'list_price', 'list_price',
+    'description', 'description',
+    'description_sale', 'description_sale',
+    'default_code', 'default_code',
+    'image', 'image_1920',
+}
 
 # fields = [
 #               'active','description','description_sale','image',
@@ -94,29 +40,17 @@ fields = [
 #               'website_description',
 #               'website_published','public_desc', 'reseller_desc']
 
-# nfields = fields + ['old_id']
-
 source = odoorpc.ODOO(host=source_params["host"],port=source_params["port"])
 source.login(source_params["db"],login=source_params["user"],password=source_params["password"])
+
 target = odoorpc.ODOO(host=target_params["host"],port=target_params["port"])
 target.login(target_params["db"],login=target_params["user"],password=target_params["password"])
 
-parents=[]
-print('hej')
-# for target_id in target.env['product.template'].search([]):
-#   target_product = target.env['product.template'].browse(target_id)
-  # target_product.unlink()
+print('unlinking existsing products ...')
+for target_id in target.env['product.template'].search([]):
+  target.env['product.template'].browse(target_id).unlink()
 
-
-# target_product = target.env['product.template'].create({'name': 'sandra'})
-# print(target_product)
-
+print('transferring products from source ...')
 for source_id in source.env['product.template'].search([]):
-  print ('hej')
-  # source_product = source.env['product.template'].read(source_id, fields)
-  # target_product = target.env['product.template'].create({key:source_product[key] for key in fields})
-  target_product.image_1920 = source_id.mapped('image_medium')
-
-  target.env['product.template'].write(target_product, {'image_1920': source_product['image_medium']})
-  # target_product = target.env['product.template'].create({key:source_product[key] for key in fields})
-  # target_product.old_id = source_id
+    source_product = source.env['product.template'].read(source_id, fields)
+    target.env['product.template'].create({fields[key] : source_product[key] for key in fields.keys()})
