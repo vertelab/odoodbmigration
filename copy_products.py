5a835b4c6a9e2d3b6b92f047933620862627dea8
Lukas Pohlman <pohluk@barney.vertel.se>
2021-01-19 12:12:03 +0000

    Bugs overload

--- copy_products.py
+++ copy_products.py
@@ -29,7 +29,7 @@ source_params = {
 target_params = {
             "host" : "81.170.214.150",
             "port" : 8069,
-            "db"   : "maria_demo",
+            "db"   : "maria_nodemo",
             "user" : "admin",
             "password"  : "admin"
         }
@@ -41,7 +41,7 @@ target = odoorpc.ODOO(host=target_params["host"],port=target_params["port"])
 target.login(target_params["db"],login=target_params["user"],password=target_params["password"])
 
 # variant fields to copy from source to target
-# 'source_field_name' : 'target_field_name'
+# { 'source_field_name' : 'target_field_name' }
 variant_fields = {
     'name' : 'name',
     'sale_ok' : 'sale_ok', 
@@ -51,11 +51,11 @@ variant_fields = {
     'description_sale': 'description_sale',
     'default_code': 'default_code',
     'image_medium' : 'image_1920',
-    'id' : 'old_id',
+    #'id': 'old_id',
 }
 
 # template fields to copy from source to target
-# 'source_field_name' : 'target_field_name'
+# { 'source_field_name' : 'target_field_name' }
 template_fields = {
     'name' : 'name',
     'sale_ok' : 'sale_ok', 
@@ -66,78 +66,81 @@ template_fields = {
     'default_code': 'default_code',
     'image_medium' : 'image_1920',
     'website_published': 'website_published',
-    'id' : 'old_id',
+    #'id': 'old_id',
 }
 
 # category fields to copy from source to target
-# 'source_field_name' : 'target_field_name'
+# { 'source_field_name' : 'target_field_name' }
 category_fields = {
     'name' : 'name', 
     'display_name' : 'display_name',
 }
 
-# Structures to lookup the new target id from the old source id.
-# old_id : new_id
+# Structures to lookup the new target id from the old source id. { old_id : new_id }
 templates_id = {}
-variants_id = {}
 categories_id = {}
 
+target.config['auto_commit'] = False
+
+# delete all records in model
+def unlink(model):
+    print('unlinking ' + model + ' ...')
+    try:
+        target.env['product.public.category'].browse(target.env['product.public.category'].search([])).unlink()
+    except:
+        print(model + ' already unlinked')
+
 # UNLINKS
-print('unlinking existing categories ...')
-target.env['product.public.category'].browse(target.env['product.public.category'].search([])).unlink()
-
-print('unlinking existing templates ...')
-target.env['product.template'].browse(target.env['product.template'].search([])).unlink()
-
-print('unlinking existing variants ...')
-target.env['product.product'].browse(target.env['product.product'].search([])).unlink()
+unlink('product.public.category')
+unlink('product.template')
+unlink('product.product')
 
 # TEMPLATES
 print('copying templates from source to target ...')
-for source_id in source.env['product.template'].search([]):
-    source_template = source.env['product.template'].read(source_id, template_fields)
-    target_template = target.env['product.template'].create({template_fields[key] : source_template[key] for key in template_fields.keys()})
-    templates_id[source_id] = target_template
-    print("created product.template id", target_template)
-
-# VARIANTS
-print('copying variants from source to target ...')
-for source_id in source.env['product.product'].search([]):
-    source_variant = source.env['product.product'].read(source_id, variant_fields)
-    target_variant = target.env['product.product'].create({variant_fields[key] : source_variant[key] for key in variant_fields.keys()})
-    variants_id[source_id] = target_variant
-    print("created product.product id", target_variant)
-
-print('adding variants to templates ...')
-for source_id in source.env['product.template'].search([]):
-    source_template = source.env['product.template'].browse(source_id)
-    # loop thourgh source template variants and copy them to target
-    for source_template_variant in source_template.product_variant_ids.ids:
-        target_template = target.env['product.template'].browse(templates_id[source_id])
-        target_template.product_variant_ids += variants_id[source_id]
-        print("added variant to", target_template.id)
+for source_template_id in source.env['product.template'].search([]):
+    source_template = source.env['product.template'].browse(source_template_id)
+    target_template_id = target.env['product.template'].create({template_fields[key] : source_template[key] for key in template_fields.keys()})
+    target_template = target.env['product.template'].browse(target_template_id)
+    print("created product.template id", target_template_id)
+    
+    # copy its variants
+    target_variants = []
+    for source_variant_id in source_template.product_variant_ids.ids:
+        source_variant = source.env['product.product'].read(source_variant_id, variant_fields)
+        target_variants.append(target.env['product.product'].create({variant_fields[key] : source_variant[key] for key in variant_fields.keys()}))
+        
+    try:
+        target_template.product_variant_ids = [(6, 0, target_variants)]
+        print("created product.product ids", target_variants)
+    except:
+        print("variant already exists ...")
+    
+    templates_id[source_template_id] = target_template
+target.env.commit()
 
 # CATEGORIES
 print('copying categories from source to target ...')
 for source_category_id in source.env['product.public.category'].search([]):
-    source_category = source.env['product.public.category'].browse(source_category_id, category_fields)
+    source_category = source.env['product.public.category'].browse(source_category_id)
     target_category = target.env['product.public.category'].create({category_fields[key] : source_category[key] for key in category_fields.keys()})
     categories_id[source_category_id] = target_category
     print('created category', target_category)
+target.env.commit()
 
 print('adding categories to target templates ...')
 for source_template_id in source.env['product.template'].search([]):
     source_template = source.env['product.template'].browse(source_template_id)
-    # loop through the templates categories and copy them to target
-    for category in source_template.public_categ_ids:
-        target_category_id = category_id[category.id]
-        target_template = target.env['product.template'].browse(templates_id[source_template_id])
-        target_template.public_categ_ids += target_category_id
-        print('added category to template', target_template.id)
+    target_template = target.env['product.template'].browse(templates_id[source_template_id])[0]
+    # get the source template categories and write them to target
+    target_template.public_categ_ids = [(6, 0, [categories_id[category] for category in source_template.public_categ_ids.ids])]
+    print('added category to', target_template.name)
+target.env.commit()
 
 print('adding parent_id to categories ...')
 for source_category_id in source.env['product.public.category'].search([]):
     source_category = source.env['product.public.category'].browse(source_category_id)
-    target_category = target.env['product.public.category'].browse(categories_id[source_category_id])
-    target_category.parent_id += categories_id[source_category.parent_id]
-    print('added parent_id to', target_category.id)
+    target_category = target.env['product.public.category'].browse(categories_id[source_category_id])[0]
+    # get source category parents and write them to target
+    target_category.parent_id = [(6, 0, [categories_id[parent] for parent in source_category.parent_id.ids])]
+    print('added parent to', target_category.display_name)
+target.env.commit()
