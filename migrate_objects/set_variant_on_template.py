0255bc44f2d662933e99fb7bae4dea430717a50a
Lukas Pohlman <pohluk@barney.vertel.se>
2021-02-10 15:25:47 +0000

    small fix on set_variant_on_template

--- migrate_objects/set_variant_on_template.py
+++ migrate_objects/set_variant_on_template.py
@@ -8,9 +8,9 @@ for source_template_id in source.env['product.template'].search([]):
     
     variants_to_write = [ get_target_record_from_id('product.product', variant).id for variant in source_template['product_variant_ids'] ]
 
-    print("target template:", target_template.id, flush=True)
+    print("target template:", target_template, flush=True)
     print("variants id in target db:", variants_to_write, flush=True)
 
     target_template.product_variant_ids = [(6, 0, variants_to_write)]
     
-    print('created variants of', templates_id[source_template_id], "(" + str(curr_count) + "/" + str(count_prod_tmpl) + ")", end='\t\t\r')
+    print('added', target_template.product_variant_ids,'as variants of', target_template.id)
