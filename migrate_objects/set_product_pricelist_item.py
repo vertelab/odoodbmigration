3e6f4e0fe94e940f75e909770c7d078fd13c6038
Lukas Pohlman <pohluk@barney.vertel.se>
2021-02-10 14:48:30 +0000

    refactoring

--- migrate_objects/set_product_pricelist_item.py
+++ migrate_objects/set_product_pricelist_item.py
@@ -12,14 +12,24 @@ except ImportError:
 from configuration import *
 
 for source_pricelist_item_id in source.env['product.pricelist.item'].search([]):
-    source_pricelist_item = source.env['product.pricelist.item'].read(source_pricelist_item, ['product_id'])
-    target_pricelist_item = get_target_record_from_id('product.pricelist.item', source_pricelist_item['product_id'])
+    source_pricelist_item = source.env['product.pricelist.item'].read(source_pricelist_item_id, ['id', 'product_id'])
+    target_pricelist_item = get_target_record_from_id('product.pricelist.item', source_pricelist_item_id)
     
     if not target_pricelist_item:
-        print("did not add pricelist to product")
+        print("pricelist item not found in target")
+        continue
     
-    target_pricelist_product = get_target_record_from_id('product.product', target_pricelist_item)
+    if not source_pricelist_item['product_id']:
+        print("pricelist has no product?")
+        continue
+    
+    target_pricelist_product = get_target_record_from_id('product.product', source_pricelist_item['product_id'][0])
+    
+    if not target_pricelist_product:
+        print("product not found in target")
+        continue
+    
     target_pricelist_item.write({'product_id' : target_pricelist_product.id})
     
-    print("added pricelist to product")
+    print("wrote pricelist to product")
     
