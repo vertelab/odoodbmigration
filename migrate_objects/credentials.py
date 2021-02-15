fa12c4bb38ab5d297fde7c81e7ecb73df40cceee
Lukas Pohlman <71273642+lukaspohlman@users.noreply.github.com>
2021-02-15 11:47:00 +0100

    Create credentials.py

---
+++ migrate_objects/credentials.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# this file is included in .gitignore
+
+# usage:
+# from credentials import credentials
+# credentials['frigg']['password']
+
+credentials = {
+    'frigg' : {
+        'user' : 'admin',
+        'password' : '',
+    },
+    
+    'tor' : {
+        'user' : 'admin',
+        'password' : '',
+    },
+}
