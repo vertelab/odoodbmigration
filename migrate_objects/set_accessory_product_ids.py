81106e3bf65fa7c60e150b322cdde6e22fac63e4
Sandra Pettersson <petsan@barney.vertel.se>
2021-03-31 13:22:44 +0000

    small fix

--- migrate_objects/set_accessory_product_ids.py
+++ migrate_objects/set_accessory_product_ids.py
@@ -3,14 +3,14 @@
 from configuration import *
 
 for source_product_id in source.env['product.product'].search([]):
-    source_product = source.env['product.product'].read(source_template_id, ['id', 'accessory_product_id'])
-    target_product = get_target_record_from_id('product.product', source_product['id'])
+    source_product = source.env['product.product'].read(source_product_id, ['id', 'accessory_product_ids'])
+    target_product = get_target_record_from_id('product.product', source_product_id)
     
-    if not target_product:
-        continue
+    group_to_write = [ get_target_record_from_id('product.product', group).id for group in source_product['accessory_product_ids']]
+
+    print("target template:", target_product, flush=True)
+    print("group id in target db:", group_to_write, flush=True)
+
+    target_product.accessory_product_ids = [(6, 0, group_to_write)]
     
-    target_accessory_ids = [ get_target_record_from_id('product.product', access_id).id for access_id in source_product['accessory_product_id'] ] 
-    target_accessory.accessory_product_id = [(6, 0, target_accessorry_ids)]
-
-    print("wrote accessory_product_id to", target_product.id)
-
+    print('added', target_product.accessory_product_ids,'as access group of', target_product.id)
