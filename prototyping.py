4ea6490782f2c4e13181160a11ef078238697f3d
Simon Rundstedt <simon.rundstedt@vertel.se>
2021-05-10 15:04:35 +0200

    Graph: bokeh.io sample

--- prototyping.py
+++ prototyping.py
@@ -61,4 +61,45 @@ for d in dependencies:
     dependencies_count.sort(key=lambda E: E[1])
 print(dependencies)
 
+#%% ---------------------------------------------------------------------------
+# Modified demo from bokeh.io about networkx
+import networkx as nx
+
+from bokeh.io import output_file, show
+from bokeh.models import (BoxSelectTool,WheelZoomTool, Circle, EdgesAndLinkedNodes, HoverTool,
+                          MultiLine,NodesOnly, NodesAndLinkedEdges, Plot, Range1d, TapTool,)
+from bokeh.palettes import Spectral4
+from bokeh.plotting import from_networkx
+
+import utils.graph as graph
+
+
+plot = Plot(plot_width=2560, plot_height=1440,
+            x_range=Range1d(-1.1,1.1), y_range=Range1d(-1.1,1.1))
+plot.title.text = "Graph Interaction Demonstration"
+
+tooltips = [("Index", "@index")]
+my_hover = HoverTool(tooltips=tooltips)
+plot.add_tools(my_hover, TapTool(), BoxSelectTool(),WheelZoomTool())
+
+#G = graph.get_ordered_graph(conn,required_only=False)
+graph_renderer = from_networkx(G, nx.nx_pydot.graphviz_layout(G, prog="dot"), scale=1, center=(0,0))
+
+graph_renderer.node_renderer.glyph = Circle(size=5, fill_color=Spectral4[0])
+graph_renderer.node_renderer.selection_glyph = Circle(size=15, fill_color=Spectral4[2])
+graph_renderer.node_renderer.hover_glyph = Circle(size=15, fill_color=Spectral4[1])
+
+graph_renderer.edge_renderer.glyph = MultiLine(line_color="#CCCCCC", line_alpha=0.8, line_width=5)
+graph_renderer.edge_renderer.selection_glyph = MultiLine(line_color=Spectral4[2], line_width=5)
+graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color=Spectral4[1], line_width=5)
+
+graph_renderer.selection_policy = NodesAndLinkedEdges()
+graph_renderer.inspection_policy = NodesAndLinkedEdges()
+
+plot.renderers.append(graph_renderer)
+
+output_file("interactive_graphs.html")
+show(plot)
+#%% ---------------------------------------------------------------------------
+# Cleanup
 conn.logout()
